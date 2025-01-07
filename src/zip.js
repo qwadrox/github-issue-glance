9c9cf79ca3b0cc77715181cfd1f308e4e03b9000
Boros Máté <33902750+qwadrox@users.noreply.github.com>
2025-01-07 08:12:34 +0100

    Add format-check script to package.json and create GitHub Actions workflow for Chrome extension publishing

--- src/zip.js
+++ src/zip.js
@@ -4,7 +4,4 @@ import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 const manifest = require('../build/manifest.json')
 
-gulp
-  .src('build/**')
-  .pipe(zip(`${manifest.name.replaceAll(' ', '-')}-${manifest.version}.zip`))
-  .pipe(gulp.dest('package'))
+gulp.src('build/**').pipe(zip('github-issue-glance.zip')).pipe(gulp.dest('package'))
